c144be7026e9a9865e515d08bd1c0b8dc006b250
AyitaXD <nadia.morales.leiva@gmail.com>
2018-01-08 10:14:46 -0300

    Etapa 1: Ocultar flecha y mostrar menu del index y ocultarla en recipe

--- js/script-index.js
+++ js/script-index.js
@@ -1,5 +1,6 @@
 $(document).ready( function(){
 
+	 $('.js-back').hide();
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
 	renderHighlightedRecipes(recipesArray);
 
@@ -7,7 +8,7 @@ $(document).ready( function(){
 
 
 /*
-* Función que se encarga de pintar TODAS las recetas que tengan 
+* Función que se encarga de pintar TODAS las recetas que tengan
 * marcado el atributo "highlighted" como TRUE
 */
 function renderHighlightedRecipes(recipesArray) {
@@ -15,9 +16,9 @@ function renderHighlightedRecipes(recipesArray) {
 }
 
 /*
-* Función que se encarga de pintar UNA recetas que tenga 
+* Función que se encarga de pintar UNA recetas que tenga
 * marcado el atributo "highlighted" como TRUE
-* Aqui se tiene que crear el HTML que esta en el 
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
@@ -35,11 +36,9 @@ function renderActivities(activitiesArray) {
 
 /*
 * Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
-	
+
 }
-
-

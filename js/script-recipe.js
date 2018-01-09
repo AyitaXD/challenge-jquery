1981419704ed9286de610095eccaf1e56d443841
AyitaXD <nadia.morales.leiva@gmail.com>
2018-01-08 22:41:18 -0300

    Etapa 8: agregando y quitando la clase make

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -2,4 +2,19 @@ $(document).ready(function() {
   //oculta el icono menu del navbar de recipe
   $('.js-menu').hide();
   console.log('Etapa 7');
+  addMake();
+  removeMake();
 });
+//funcion que agrega la clase make al div page recipe
+function addMake() {
+  $('.js-show-recipe').click(function () {
+    $('#page').addClass('make');
+  });
+
+}
+//funcion que quita la clase make al div page recipe
+function removeMake() {
+  $('.js-show-make').click(function() {
+    $('#page').removeClass('make');
+  });
+}

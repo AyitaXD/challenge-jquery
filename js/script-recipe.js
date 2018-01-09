40cdef5f567b9370a1d6e2a5bfd2e20dd471bc1b
AyitaXD <nadia.morales.leiva@gmail.com>
2018-01-08 23:10:37 -0300

    Etapa 9: funcionando las pestañas

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,16 +5,25 @@ $(document).ready(function() {
   addMake();
   removeMake();
 });
-//funcion que agrega la clase make al div page recipe
+
+/*funcion que agrega la clase make al div page
+*agrega la clase active a show-recipe y se la quita show-make
+*/
 function addMake() {
   $('.js-show-recipe').click(function () {
     $('#page').addClass('make');
+    $('.js-show-recipe').addClass('active');
+    $('.js-show-make').removeClass('active');
   });
 
 }
-//funcion que quita la clase make al div page recipe
+/*funcion que quita la clase make al div page recipe
+*agrega la clase active a show-make y se la quita show-recipe
+*/
 function removeMake() {
   $('.js-show-make').click(function() {
     $('#page').removeClass('make');
+    $('.js-show-make').addClass('active');
+    $('.js-show-recipe').removeClass('active');
   });
 }
